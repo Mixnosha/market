d6f50a222d003bc9888e520940fb93592abbdaf1
mixnosha <xmixho@gmail.com>
2022-07-15 03:42:16 +0300

    models

--- main_app/market/admin.py
+++ main_app/market/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from market.models import Product, Review, Manufacturer
+
+admin.site.register(Review)
+admin.site.register(Manufacturer)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ['product_name']
+    search_fields = ['product_name']
+    fields = ['product_name', 'product_price', 'product_description', 'product_review', 'product_manufacturer', 'product_image']
+
+
--- main_app/market/models.py
+++ main_app/market/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_price = models.DecimalField(decimal_places=2, max_digits=9999999)
+    product_description = models.TextField()
+    product_manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+    product_review = models.ForeignKey('Review', on_delete=models.CASCADE, blank=True)
+    product_image = models.ImageField(upload_to='users/product_images')
+
+    def __str__(self):
+        return self.product_name
+
+
+class Review(models.Model):
+    review_user = models.CharField(max_length=100)
+    review_rating = models.IntegerField()
+    review_description = models.TextField(blank=True)
+    review_image = models.ImageField(upload_to='users/review_images', blank=True)
+
+    def __str__(self):
+        return self.review_user
+
+
+class Manufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.manufacturer_name
+
+
+
