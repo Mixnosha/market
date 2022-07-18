26e3c9eb3ce353ecdb38f9d7708096f01bf28859
mixnosha <xmixho@gmail.com>
2022-07-18 19:33:30 +0300

    main_view

--- main_app/market/views.py
+++ main_app/market/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
 
+from market.forms import SearchForm
 from market.models import Product, Category
 
 
@@ -9,6 +10,7 @@ class ProductView(ListView):
     paginate_by = 100
     context_object_name = 'product_list'
     template_name = 'market/main_page.html'
+    name_url = 'main_page'
 
     def get_queryset(self):
         queryset = Product.objects.filter(sales__gte=100)
@@ -25,6 +27,7 @@ class CategoryView(ListView):
     template_name = 'market/category.html'
     paginate_by = 25
     context_object_name = 'category_list'
+    name_url = 'cats_view'
 
     def get_queryset(self):
         slug = self.kwargs["slug"]
@@ -37,4 +40,5 @@ class CategoryView(ListView):
         slug = self.kwargs["slug"]
         cat = Category.objects.get(slug=slug)
         context['name_category'] = cat
+        context['form_search_val'] = self.name_url
         return context
