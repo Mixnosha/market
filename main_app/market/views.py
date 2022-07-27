b77b084db0a1c4004a7205728bd28617b8ed3d23
mixnosha <xmixho@gmail.com>
2022-07-28 00:53:22 +0300

    buy_all

--- main_app/market/views.py
+++ main_app/market/views.py
@@ -1,22 +1,12 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import ListView, CreateView
-from psycopg2.sql import NULL
-
+from market.business_logic import deliv_delete, get_sum_product_price_basket
 from market.forms import SearchForm, RegisterUserForms, LoginUserForm, ProfileForm
 from market.models import Product, Category, Profile, Basket, Delivery, BuyProduct
-
-
-def deliv_delete(request):
-    try:
-        buy_prod = BuyProduct.objects.filter(delivery__delivery='Delivered')
-        buy_prod.delete()
-        return redirect('profile')
-    except Exception:
-        pass
 
 
 class ProductView(ListView, View):
@@ -162,25 +152,10 @@ class BasketView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['all_price'] = get_sum_product_price_basket(self.request)
         context['title'] = 'basket'
         context['category'] = Category.objects.all()
         return context
-
-
-def add_basket(request):
-    if request.method == 'GET':
-        if request.user.is_authenticated:
-            user = Profile.objects.get(user=request.user)
-            product = Product.objects.get(id=request.GET.get('add_product_id'))
-            try:
-                new_amount = Basket.objects.get(user=user, product=product)
-                new_amount.amount = new_amount.amount + 1
-                new_amount.save()
-            except Exception:
-                Basket.objects.create(user=user, product=product, status='доставка через 3 дня')
-        else:
-            return HttpResponse('Сначало нужно войти')
-    return redirect('basket')
 
 
 class BuyProductView(ListView):
@@ -200,21 +175,5 @@ class BuyProductView(ListView):
         return context
 
 
-def buy_product_def(request):
-    deliv = Delivery.objects.get(id=1)
-    pr_id = request.GET.get('change_profile')
-    user = Profile.objects.get(user=request.user)
-    try:
-        product = Product.objects.get(id=pr_id)
-    except Exception:
-        return HttpResponse('Данный продукт уже не продается :(')
-    try:
-        del_bas = Basket.objects.get(user=user, product=product)
-        BuyProduct.objects.create(user=user, product=product, delivery=deliv, amount=del_bas.amount)
-        del_bas.delete()
-    except Exception as es:
-        print(es)
-        return HttpResponse(':(')
-    return redirect('profile')
 
 
