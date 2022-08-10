23f34b75ef64e55fedb3b605e09034b81ead6ec0
mixnosha <xmixho@gmail.com>
2022-08-10 10:55:49 +0300

    add favorite product

--- main_app/market/consumers.py
+++ main_app/market/consumers.py
@@ -15,15 +15,21 @@ class FavoriteAddConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
+        print('1')
         text_data_json = json.loads(text_data)
-        id = text_data_json['product_id']
+        pr_slug = text_data_json['product_slug']
+        print(pr_slug)
         user = text_data_json['user']
         try:
+            print('user')
             user = Profile.objects.get(user__username=user)
             fav = Favorites.objects.get(user=user)
-            fav.products.add(Product.objects.get(id=id))
-            self.send(text_data="true   ")
+            fav.products.add(Product.objects.get(slug=pr_slug))
+            print(pr_slug)
+            self.send(text_data=json.dumps({
+                'pr_slug': pr_slug,
+                'sus': 'true',
+            }))
         except Exception as e:
             print(e)
-            self.send(text_data="false")
-
+            pass
